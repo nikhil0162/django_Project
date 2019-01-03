cf55fb244b950266e73d8ca1839aae18dfa104c0
nikhil0162 <nikhil0162@gmail.com>
2019-01-03 10:45:30 +0530

    3-jan-2019

--- django_practice/urls.py
+++ django_practice/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from user import views as user_views
-from blog import views as bviews
+
 
 
 urlpatterns = [
@@ -29,6 +29,9 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name='password_reset_confirm'),
     path('', include('blog.urls')),
     path('weather/', include('weather.urls')),
     path('todo/', include('todo.urls')),
